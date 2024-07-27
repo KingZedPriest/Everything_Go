c2347b2154580a09894ae9b322e5e83274ecc00c
Priest <Charleschukwuemeka47@gmail.com>
2024-07-27 22:25:23 +0100

    Feat: Adding two variables together in a function, and returning it

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
-// Create a new function
+// Create a new function, the entry point
+func intDiv(no1, no2 int) int {
+	result := no1 + no2
+	return result
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World. \n", rand.Intn(200))
+	newVar := "text"
+	fmt.Println(newVar)
+	result := intDiv(2, 5)
+	fmt.Printf("THis is the result %v", result)
 }
