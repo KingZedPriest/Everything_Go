482755d0ad45486bf25ce92885410f7f9feafce7
Priest <Charleschukwuemeka47@gmail.com>
2024-07-27 22:58:24 +0100

    Feat: Add error handling in the code

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 )
 
 // Create a new function, the entry point
-func intDiv(no1, no2 int) int {
-	result := no1 + no2
-	return result
-}
-
 func main() {
 	fmt.Println("Hello World. \n", rand.Intn(200))
 	newVar := "text"
 	fmt.Println(newVar)
-	result := intDiv(2, 5)
+	result, err := intDiv(2, 5)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
 	fmt.Printf("THis is the result %v", result)
+}
+
+func intDiv(no1, no2 int) (int, error) {
+	var err error
+	result := no1 + no2
+	if result == 0 {
+		err = errors.New("The result is zero")
+		return 0, err
+	}
+	return result, err
 }
